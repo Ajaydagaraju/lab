ee596f4a8839ad47793b22231f942289fb6b1449
Manish Chaudhary <manish@Manish.local>
2026-02-25 00:42:22 +0530

    build issue fixed

--- app/(routes)/library-information/view/page.jsx
+++ app/(routes)/library-information/view/page.jsx
@@ -1,20 +1,34 @@
+// "use client";
+
+// import { useSearchParams } from "next/navigation";
+
+// export default function IframePage() {
+//   const searchParams = useSearchParams();
+//   const url = searchParams.get("url");
+
+//   if (!url) return <div>No URL Provided</div>;
+
+//   return (
+//     <div className="min-h-screen bg-black">
+//       <iframe
+//         src={url}
+//         className="w-full h-screen"
+//         title="External Resource"
+//       />
+//     </div>
+//   );
+// }
+
+
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import IframeContent from "./IframeContent";
 
-export default function IframePage() {
-  const searchParams = useSearchParams();
-  const url = searchParams.get("url");
-
-  if (!url) return <div>No URL Provided</div>;
-
+export default function Page() {
   return (
-    <div className="min-h-screen bg-black">
-      <iframe
-        src={url}
-        className="w-full h-screen"
-        title="External Resource"
-      />
-    </div>
+    <Suspense fallback={<div className="p-10">Loading...</div>}>
+      <IframeContent />
+    </Suspense>
   );
 }
